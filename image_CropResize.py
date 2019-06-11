6d0c2a96a289b6a79770a7b4e24489e006830179
avasqu27 <35350821+avasqu27@users.noreply.github.com>
2019-06-11 14:14:51 -0700

    Updated CropResize

--- image_CropResize.py
+++ image_CropResize.py
@@ -1,31 +1,29 @@
 
 #Crop and Resize a image 
-#inputs:   image - a numpy array of the original image
-#           bbsize - a list of bounding box and new size information
-#           (bbsize[0], bbsize[1]) - starting point of the bounding box (x1, y1)
-#           (bbsize[2], bbsize[3]) - ending point of the bounding box (x2, y2) 
-#           bbsize[4]  - the width of the output image
-#           bbsize[5] - the height of the output image
  
 import numpy as np
 import cv2
 def cropResizeImage(image, bbsize):
-    #x1 = bbsize[0]
-    #y1 = bbsize[1]
-    #x2 = bbsize[2]
-    #y2 = bbsize[3]
-    #w = list[4]
-    #h = list[5]
-    crop = image[bbsize[0]:bbsize[1], bbsize[2]:bbsize[3]]
-    # For testing purposes:
-    #print("* Printing crop")
-    #print(crop)
-  
-    #Resizing
-    #INTER_CUBIC - a bicubic interpolation over 4x4 pixel neighborhood
-  
-    dim = (bbsize[4],bbsize[5])
-    # For testing purposes: 
-    #print(dim)
-    new_image = cv2.resize(crop, dim, interpolation =cv2.INTER_CUBIC)
+       # taken from image_manipulation.py by Conor Green, LMU EE
+
+    # print("* Original image shape is: ")
+    # print(image.shape)
+    # resize using cv2 function 
+    # cv2.INTER_AREA is used to shrink image 
+    image = cv2.resize(image, (140,220), interpolation =cv2.INTER_AREA)
+    # print("* New image shape is: ")
+    # print(image.shape)
+    
+    # defining length and heights in preparation for cropping 
+    xl = 360
+    xh = 600
+    yl = 540
+    yh = 400
+    new_image = image[xl:xh, yl:yh, :]
+    
+    # For clarification on the typings: 
+    # print("* Cropped image's type is: ")
+    # print(type(new_image))
+    # print("* Cropped image shape is: ")
+    # print(crop_im.shape)
     return new_image
