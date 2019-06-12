c7360c18c555491650964e93033596dea730c963
avasqu27 <35350821+avasqu27@users.noreply.github.com>
2019-06-12 10:26:40 -0700

    Updated length and heights

--- image_CropResize.py
+++ image_CropResize.py
@@ -3,27 +3,27 @@
  
 import numpy as np
 import cv2
-def cropResizeImage(image, bbsize):
-       # taken from image_manipulation.py by Conor Green, LMU EE
 
-    # print("* Original image shape is: ")
-    # print(image.shape)
-    # resize using cv2 function 
-    # cv2.INTER_AREA is used to shrink image 
-    image = cv2.resize(image, (140,220), interpolation =cv2.INTER_AREA)
-    # print("* New image shape is: ")
-    # print(image.shape)
-    
-    # defining length and heights in preparation for cropping 
-    xl = 360
-    xh = 600
-    yl = 540
-    yh = 400
-    new_image = image[xl:xh, yl:yh, :]
-    
-    # For clarification on the typings: 
-    # print("* Cropped image's type is: ")
-    # print(type(new_image))
-    # print("* Cropped image shape is: ")
-    # print(crop_im.shape)
-    return new_image
+def cropResizeImage(image, desired_h, desired_w):
+ # taken from image_manipulation.py by Conor Green, LMU EE
+ 
+ # print("* Original image shape is: ")
+ # print(image.shape)
+ 
+ # resize using cv2 function 
+ # cv2.INTER_AREA is used to shrink image 
+ image = cv2.resize(image, (140,220), interpolation =cv2.INTER_AREA)
+ 
+ # defining lengths and heights in preparation for cropping 
+ xl = 120
+ xh = 360
+ yl = 120
+ yh = 360
+ crop_im = image[xl:xh, yl:yh, :]
+ 
+ # For clarification on the typings: 
+ # print("* Cropped image's type is: ")
+ # print(type(crop_im))
+ # print("* Cropped image shape is: ")
+ # print(crop_im.shape)
+ return (crop_im)
