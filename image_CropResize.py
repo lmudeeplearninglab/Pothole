1d5e27c62a7f1795c84d998e93129a66d5ef5d2f
eherna48 <49418794+eherna48@users.noreply.github.com>
2019-06-19 11:43:15 -0700

    Update image_CropResize.py

--- image_CropResize.py
+++ image_CropResize.py
@@ -1,29 +1,27 @@
-
 #Crop and Resize a image 
  
 import numpy as np
-import cv2
-
-def cropResizeImage(image, desired_h, desired_w):
- # taken from image_manipulation.py by Conor Green, LMU EE
- 
- # print("* Original image shape is: ")
- # print(image.shape)
- 
- # resize using cv2 function 
- # cv2.INTER_AREA is used to shrink image 
- image = cv2.resize(image, (140,220), interpolation =cv2.INTER_AREA)
- 
+import cv2 
+import image_segmentation as isp
  # defining lengths and heights in preparation for cropping 
- xl = 120
- xh = 360
- yl = 120
- yh = 360
- crop_im = image[xl:xh, yl:yh, :]
  
- # For clarification on the typings: 
- # print("* Cropped image's type is: ")
- # print(type(crop_im))
- # print("* Cropped image shape is: ")
- # print(crop_im.shape)
- return (crop_im)
+def cropResizeImage(image, bounds, desired_h, desired_w):
+    # taken from image_manipulation.py by Conor Green, LMU EE
+ 
+    # print("* Original image shape is: ")
+    # print(image.shape)
+    xl = bounds[0]
+    xh = bounds[2]
+    yl = bounds[1]
+    yh = bounds[3]
+    crop_im = image[xl:xh, yl:yh, :]
+    # resize using cv2 function 
+    # cv2.INTER_AREA is used to shrink image 
+    img = cv2.resize(image, (140,220), interpolation =cv2.INTER_AREA)
+    # For clarification on the typings: 
+    # print("* Cropped image's type is: ")
+    # print(type(crop_im))
+    # print("* Cropped image shape is: ")
+    # print(crop_im.shape)
+    return (img)
+
