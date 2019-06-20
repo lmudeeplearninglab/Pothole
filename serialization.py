4d0ba815fd47cb1ba0ccbaa10e936673a095c0bf
avasqu27 <35350821+avasqu27@users.noreply.github.com>
2019-06-20 12:13:36 -0700

    Update serialization.py

--- serialization.py
+++ serialization.py
@@ -13,50 +13,41 @@ print('* Successfully ran the showing...')
 #testing(img)
 
 def preprocessing(img): 
-    #from ericson and casey's file: 
-    # set image to result of the file 
-    print(type(img))
-    # set random values to form a square bound 
-    bbsize = [400, 400, 620, 620, 200, 200]
-    img = icr.cropResizeImage(img, bbsize)
-    print(type(img))
+    #from image_CropResize file:
+    # 120 and 320 are random input values; must be replaced with the exact 
+    #  coordinates using Conor's extract_road function 
+    image = icr.cropResizeImage(image, 120,320)
+    # Crop and Resize snippet from Conor's code 
+    # for testing purposes only: 
+    # print(type(img))
     return img 
 
 def serialize_images(dir): 
     # sets first directory: training images (but can be changed in the real program)
     directory = dir + "/training/" 
-    print("* Directory TYPE is ")
-    print( type(directory))
-    print("* Directory is ")
-    print( directory)
+    # print("* Directory TYPE is ")
+    # print( type(directory))
+    # print("* Directory is ")
+    # print( directory)
     direct_images = os.listdir(directory)
-    print("* Direct_images TYPE is ")
-    print(type(direct_images))
-    print("* Direct_images is ")
-    print( direct_images)
+    # print("* Direct_images TYPE is ")
+    # print(type(direct_images))
     
     # setting up the lists to create dictionary
-    print("* Setting up lists...")
     images = [] 
     labels = [] 
-    print("* Starting for loop...")
     count = 0
     
     for image in direct_images: 
         # read the image using directory 
-        print("* In For loop")
-        count = count + 1 
-        print("* Count is ")
-        print(count)
-        image = cv2.imread(direct_images[count])
-        #image = cv2.imread(direct_images + image)
+        image = cv2.imread(directory + image)
         # preprocess image 
         image = preprocessing(image)
+        # for testing purposes: 
+        # print(type(image))
+        
         # add to corresponding list 
         images.append(image) 
         labels.append(1)
-    
-    print("* Out of For loop")
-    print(type(images))
-    print(type(labels))
+    return (images, labels) 
 
